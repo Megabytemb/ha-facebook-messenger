bfad8b7dbc0f182985404774f560f73644987fa6
Michael Benz <michael@benz.io>
2024-01-06 15:26:37 +1100

    feat: update release pipeline

--- custom_components/facebook_messenger/const.py
+++ custom_components/facebook_messenger/const.py
@@ -2,8 +2,7 @@
 
 ################################
 # Do not change! Will be set by release workflow
-INTEGRATION_VERSION = "main"  # git tag will be used
-MIN_REQUIRED_HA_VERSION = "0.0.0"  # set min required version in hacs.json
+INTEGRATION_VERSION = "1.0.0"  # x-release-please-version
 ################################
 
 NAME = "Facebook Messenger"
